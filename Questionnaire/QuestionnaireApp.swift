d9ff95dbefade1bd3355806c78c199550313e211
Kilorafix <kilorafix@gmail.com>
2022-11-07 19:16:57 +0100

    Initial Commit

---
+++ Questionnaire/QuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionnaireApp.swift
+//  Questionnaire
+//
+//  Created by Kilorafix on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
